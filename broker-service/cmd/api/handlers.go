6dba852eec53c33cfa3f81c51899d278afcdd380
jakebuceac <jake.buceac@in2tel.ie>
2024-12-18 13:33:12 +0000

    Added mongo database connection and logger service

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -10,11 +10,17 @@ import (
 type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
+	Log    LogPayload  `json:"log,omitempty"`
 }
 
 type AuthPayload struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LogPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
 }
 
 func (app *Config) Broker(response http.ResponseWriter, request *http.Request) {
@@ -40,9 +46,49 @@ func (app *Config) HandleSubmission(response http.ResponseWriter, request *http.
 	switch requestPayload.Action {
 	case "auth":
 		app.authenticate(response, requestPayload.Auth)
+	case "log":
+		app.logItem(response, requestPayload.Log)
 	default:
-		app.errorJSON(response, errors.New("Unknown action!"))
+		app.errorJSON(response, errors.New("unknown action"))
 	}
+}
+
+func (app *Config) logItem(response http.ResponseWriter, logPayload LogPayload) {
+	jsonData, _ := json.MarshalIndent(logPayload, "", "\t")
+	logServiceUrl := "http://logger-service/log"
+	request, err := http.NewRequest("POST", logServiceUrl, bytes.NewBuffer(jsonData))
+
+	if err != nil {
+		app.errorJSON(response, err)
+
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	clientResponse, err := client.Do(request)
+
+	if err != nil {
+		app.errorJSON(response, err)
+
+		return
+	}
+
+	defer clientResponse.Body.Close()
+
+	if clientResponse.StatusCode != http.StatusAccepted {
+		app.errorJSON(response, err)
+
+		return
+	}
+
+	var payload jsonResponse
+
+	payload.Error = false
+	payload.Message = "Logged!"
+
+	app.writeJSON(response, http.StatusAccepted, payload)
 }
 
 func (app *Config) authenticate(response http.ResponseWriter, authPayload AuthPayload) {
